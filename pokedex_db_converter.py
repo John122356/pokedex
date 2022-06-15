e5f59d1ab91dd652d54a16800590236b21ae7feb
unknown <john.lener@cix.csi.cuny.edu>
2022-06-15 14:01:20 -0400

    Added comments for each function.

--- pokedex_db_converter.py
+++ pokedex_db_converter.py
@@ -1,8 +1,12 @@
+'''This takes the data from the mongodb database made with 
+pokedex_scraper.py and puts them into a sqlite database.
+'''
 from pymongo import MongoClient
 import sqlite3
 
+
 def create_sqlite_db(db_name):
-    ''' Create the sqlite database and its tables.'''
+    ''' Create a Pokedex sqlite database and its tables.'''
     con = sqlite3.connect(db_name)
     cur = con.cursor()
 
@@ -65,41 +69,57 @@ def create_sqlite_db(db_name):
 
 
 def insert_pokemon_table(cur, pkmn):
+    '''Insert a pokemon's name and number into the pokemon table.'''
     values = (pkmn['name'], pkmn['number'])
     print(values)
     cur.execute("INSERT INTO pokemon VALUES (?, ?);", values)
 
+
 def insert_formes_table(cur, pkmn):
+    '''Insert the details of each form of a pokemon into the formes table.'''
     for form in pkmn['formes']:
         type1 = form['types'][0]
         type2 = None if len(form['types']) < 2 else form['types'][1]
         male = 1 if 'Male' in form['gender'] else 0
         female = 1 if 'Female' in form['gender'] else 0
         values = (pkmn['name'], form['form'], form['height'], form['weight'],
-                form['category'], type1, type2, male, female)
-        cur.execute("INSERT INTO formes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);", values)
+                    form['category'], type1, type2, male, female)
+        cur.execute("INSERT INTO formes VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+                    values)
+
 
 def insert_form_descriptions_table(cur, pkmn):
+    '''Insert blurbs about each form of a pokemon into the 
+    form_descriptions table.
+    '''
     for form in pkmn['formes']:
         desc1 = form['descriptions'][0]
         values = [(pkmn['name'], form['form'], desc1)]
-        # Each form has 2 descriptions.
-        # But they might be the same and break the database's unique constraint.
+        # Each form has 2 descriptions. But they might be the same and
+        # break the database's unique constraint.
         desc2 = form['descriptions'][1]
         if desc1 != desc2:
             values.append((pkmn['name'], form['form'], desc2))
-        cur.executemany("INSERT INTO form_descriptions VALUES (?, ?, ?);", values)
-        
+        cur.executemany("INSERT INTO form_descriptions VALUES (?, ?, ?);",
+                        values)
+
+
 def insert_abilities_table(cur, ability):
+    '''Insert an ability and its description into the abilities table.'''
     # Check if the ability is already in the database.
-    cur.execute("SELECT * FROM abilities WHERE ability=?;", (ability,))
+    cur.execute("SELECT * FROM abilities WHERE ability=?;",
+                (ability['ability'],))
     if len(cur.fetchall()) == 0:
         # Insert the ability.
         name = ability['ability']
         info = ability['description']
         cur.execute("INSERT INTO abilities VALUES(?, ?);", (name, info))
 
+
 def insert_form_abilities_table(cur, pkmn):
+    '''Insert the abilities that each form of a pokemon can have into 
+    the form_abilities table.
+    '''
     values = []
     for form in pkmn['formes']:
         if 'abilities' in form.keys(): # One pokemon doesn't have abilities.
@@ -107,7 +127,9 @@ def insert_form_abilities_table(cur, pkmn):
                 values.append((pkmn['name'], form['form'], ability))
     cur.executemany("INSERT INTO form_abilities VALUES (?, ?, ?);", values)
 
+
 def insert_evolutions_table(cur, pkmn):
+    '''Insert the evolution line of this pokemon into the evolutions table.'''
     # Check if this pokemon's evolution line is already in the database.
     cur.execute("SELECT * FROM evolutions WHERE pokemon=? OR evolves_to=?;",
                 (pkmn['name'], pkmn['name']))
@@ -119,12 +141,13 @@ def insert_evolutions_table(cur, pkmn):
         evos = {}
         for dic in evos_list:
             evos.update(dic)
-        # I think first, middle, and last are the only possible evolution spots.
-        # But I'm going to make sure with an assertion.
+        # I think first, middle, and last are the only possible 
+        # evolution spots. But I'm going to make sure with an assertion.
         for key in evos.keys():
-            assert(key in ['first', 'middle', 'last'])
-        # If there are "middle" pokemon, then "first" evolves into "middle",
-        # and "middle" evolves into "last".
+            assert key in ['first', 'middle', 'last'], \
+                    "Unknown evolution spot: " + key
+        # If there are "middle" pokemon, then "first" evolves into
+        # "middle", and "middle" evolves into "last".
         if 'middle' in evos.keys():
             for mid_evo in evos['middle']:
                 for first_evo in evos['first']:
@@ -139,7 +162,6 @@ def insert_evolutions_table(cur, pkmn):
                 for first_evo in evos['first']:
                     values.append((first_evo, last_evo))
         cur.executemany("INSERT INTO evolutions VALUES (?, ?);", values)
-
 
 
 def fill_sqlite_db(db_name):
@@ -159,15 +181,18 @@ def fill_sqlite_db(db_name):
         insert_form_descriptions_table(cur, pkmn)
         insert_form_abilities_table(cur, pkmn)
         insert_evolutions_table(cur, pkmn)
-
-        '''if pkmn['name'] == 'Eevee':
-            break'''
     con.commit()
     con.close()
 
-db_name = 'pokedex.db'
-create_sqlite_db('testing.db')
-fill_sqlite_db('testing.db')
+
+def main():
+    '''Create a sqlite pokedex database and 
+    insert the data from the mongodb pokedex into it.
+    '''
+    db_name = 'pokedex.db'
+    create_sqlite_db(db_name)
+    fill_sqlite_db(db_name)
 
 
-print('done')
+if __name__ == "__main__":
+    main()
